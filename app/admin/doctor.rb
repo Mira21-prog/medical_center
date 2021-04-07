5c2b0ec19043c8eecc95530ee7ff1736ecfc1b56
Mira <matsakovamira156@gmail.com>
2021-04-07 20:18:55 +0300

    Refactoring admin panel for user and doctor

--- app/admin/doctor.rb
+++ app/admin/doctor.rb
@@ -1,11 +1,11 @@
-ActiveAdmin.register User do
+ActiveAdmin.register Doctor do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
+  #
   permit_params :email, :password, :full_name, :phone_number, :password_confirmation
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :full_name, :phone_number
   #
   # or
   #
@@ -23,7 +23,7 @@ ActiveAdmin.register User do
     column :password
     actions
   end
-  
+
   form do |f|
     f.inputs "Create doctor" do
       f.input :full_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_151105) do
+ActiveRecord::Schema.define(version: 2021_04_07_154341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_151105) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.string "phone_number"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
