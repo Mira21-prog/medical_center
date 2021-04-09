9830b2580b50e392ff0ee7ff9d8b073a728ef22e
Mira <matsakovamira156@gmail.com>
2021-04-10 00:48:27 +0300

    Add category to admin

---
+++ app/admin/categories.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Category do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  index do
+    selectable_column
+    id_column
+    column :name
+    actions
+  end
+
+  form do |f|
+    f.inputs "Create doctor" do
+      f.input :name
+      f.submit
+    end
+  end
+
+end
